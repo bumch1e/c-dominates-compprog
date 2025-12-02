d06d41263d27b42d46de190c3a5554250af6afa4
bechever <bechever@alumnos.inf.utfsm.cl>
2025-12-02 18:43:23 -0300

    feat: solution for weird algorithm

---
+++ cses/problem-set/introductory-problems/weird-algorithm/weird-algorithm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define ll long long 
+
+/**
+ * @brief solve for weird-algorithm problem
+ *
+ * The "weird algorithm" is another name for collatz's conjecture.
+ * The implementation is trivial.
+ *
+ * @return success code
+ */
+int main(){
+    ll n;
+    scanf("%lld", &n);
+
+    printf("%lld ", n);
+
+    while (n != 1) {
+        if (n % 2 == 0) 
+            n = n/2;
+        else 
+            n = 3*n + 1;
+        printf("%lld ", n);
+    }
+
+    return 0;
+}
